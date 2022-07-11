4e66c8a84e912c2f89f327205f41a26f6201fbd6
Bregy Malpartida <bregy@minsky.cc>
2022-07-11 00:54:27 -0500

    feat: dynamic illustrations

--- islands/App.tsx
+++ islands/App.tsx
@@ -7,19 +7,29 @@ import CellularAutomaton from "../components/CellularAutomaton.tsx";
 
 type AppProps = {};
 
+const illustrations = [
+  "/totemiq/ruinas_black.png",
+  "/totemiq/ruinas_blue.png",
+  "/totemiq/ruinas_rose.png",
+  "/totemiq/ruinas_lila.png",
+];
+
 export default function App({}: AppProps) {
   const [isReady, setIsReady] = useState(false);
+
+  const imageSource =
+    illustrations[Math.floor(Math.random() * illustrations.length)];
 
   return (
     <div>
       {isReady ? (
+        // <div
+        //   class={tw`z-0 absolute top-0 left-0 w-screen h-screen bg-gradient-to-t from-secondary via-primary to-primary`}
+        // />
         <div
           class={tw`z-0 absolute top-0 left-0 w-screen h-screen bg-gray-400`}
         />
       ) : (
-        // <div
-        //   class={tw`z-0 absolute top-0 left-0 w-screen h-screen bg-gradient-to-t from-secondary via-primary to-primary`}
-        // />
         <div class={tw`z-20 absolute top-0 left-0 w-screen h-screen bg-black`}>
           <div class={tw`flex justify-center`}>
             <div class={tw`h-screen flex flex-col justify-center`}>
@@ -33,10 +43,7 @@ export default function App({}: AppProps) {
           class={tw`mx-auto max-w-screen-md flex flex-col justify-between h-screen`}
         >
           <div class={tw`h-1`} />
-          <img
-            src="/totemiq/ruinas_transparent.png"
-            class={tw`max-h-screen object-contain`}
-          />
+          <img src={imageSource} class={tw`max-h-screen object-contain`} />
         </div>
       </div>
       <CellularAutomaton onReady={() => setIsReady(true)} />
